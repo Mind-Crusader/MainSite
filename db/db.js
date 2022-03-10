cd02c9324d3067b25d4054e98d3d0105eaa58770
Ranajit Nath <ranajitnath7@gmail.com>
2022-03-10 12:50:52 +0530

    V2 Files added

---
+++ db/db.js
@@ -0,0 +1,71 @@
+export default {
+	skills: [
+		{
+			title: "Languages",
+			skillName: "Python",
+			color: "1",
+			percentage: "70",
+		},
+		{
+			title: "Backend",
+			skillName: "Django",
+			color: "2",
+			percentage: "55",
+		},
+		{
+			title: "Cloud",
+			skillName: "OpenShift",
+			color: "3",
+			percentage: "90",
+		},
+		{
+			title: "Design",
+			skillName: "HTML, Bootstrap, SCSS",
+			color: "4",
+			percentage: "70",
+		},
+		{
+			title: "Version Control",
+			skillName: "GitHub",
+			color: "7",
+			percentage: "70",
+		},
+		{
+			title: "Editor",
+			skillName: "JetBrains",
+			color: "6",
+			percentage: "70",
+		},
+	],
+	footer: [
+		{
+			label: "Profiles",
+			data: [
+				{
+					text: "GitHub",
+					link: "https://github.com/vinaysomawat",
+				},
+			],
+		},
+		{
+			label: "Resources",
+			data: [
+				{
+					text: "Enable Dark/Light Mode",
+					func: "enableDarkMode()",
+				},
+				{
+					text: "Print this page",
+					func: "window.print()",
+				},
+			],
+		},
+		{
+			label: "copyright-text",
+			data: [
+				"Made with &hearts; by Vinay Somawat.",
+				"&copy; No Copyrights. Feel free to use this template."
+			],
+		},
+	],
+};
